11041ee34796652802bea5892d708b7d61ae3881
Kalpana Patidar <kalpanamds1105@gmail.com>
2025-11-14 09:44:22 -0800

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
